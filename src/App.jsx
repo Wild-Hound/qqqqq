3e2af904070e93835fd250f1a586ed384a5a9e17
Yasin Khan <yasinkhan@Yasins-Mac-mini.local>
2024-12-27 09:49:18 +0600

    image slider

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import { Image, ScrollControls, Scroll, useScroll } from "@react-three/drei";
 import { proxy, useSnapshot } from "valtio";
 import { easing } from "maath";
 
+const state = proxy({
+  clicked: null,
+});
+
 const material = new THREE.LineBasicMaterial({ color: "white" });
 const geometry = new THREE.BufferGeometry().setFromPoints([
   new THREE.Vector3(0, -0.5, 0),
@@ -61,6 +65,61 @@ function Minimap() {
   );
 }
 
+function Item({ index, position, scale, c = new THREE.Color(), ...props }) {
+  const ref = useRef();
+  const scroll = useScroll();
+  const { clicked } = useSnapshot(state);
+  const [hovered, hover] = useState(false);
+  const click = () => setClicked(() => (index === clicked ? null : index));
+  const over = () => hover(true);
+  const out = () => hover(false);
+
+  useFrame((state, delta) => {
+    const y = scroll.curve(
+      index / urls.length - 1.5 / urls.length,
+      4 / urls.length
+    );
+    easing.damp3(
+      ref.current.scale,
+      [clicked === index ? 4.7 : scale[0], clicked === index ? 5 : 4 + y, 1],
+      0.15,
+      delta
+    );
+    ref.current.material.scale = [ref.current.scale.x, ref.current.scale.y];
+    if (clicked !== null && index < clicked)
+      easing.damp(ref.current.position, "x", position[0] - 2, 0.15, delta);
+    if (clicked !== null && index > clicked)
+      easing.damp(ref.current.position, "x", position[0] + 2, 0.15, delta);
+    if (clicked === null || clicked === index)
+      easing.damp(ref.current.position, "x", position[0], 0.15, delta);
+    easing.damp(
+      ref.current.material,
+      "grayscale",
+      hovered || clicked === index ? 0 : Math.max(0, 1 - y),
+      0.15,
+      delta
+    );
+    easing.dampC(
+      ref.current.material.color,
+      hovered || clicked === index ? "white" : "#aaa",
+      hovered ? 0.3 : 0.15,
+      delta
+    );
+  });
+
+  return (
+    <Image
+      ref={ref}
+      {...props}
+      position={position}
+      scale={scale}
+      onClick={click}
+      onPointerOver={over}
+      onPointerOut={out}
+    />
+  );
+}
+
 function Items({ w = 0.7, gap = 0.15 }) {
   const { width } = useThree((state) => state.viewport);
   const xW = w + gap;
@@ -69,6 +128,11 @@ function Items({ w = 0.7, gap = 0.15 }) {
   return (
     <ScrollControls horizontal damping={0.1} pages={pages}>
       <Minimap />
+      <Scroll>
+        {
+          urls.map((url, i) => <Item key={i} index={i} position={[i * xW, 0, 0]} scale={[w, 4, 1]} url={`${url}?not-from-cache-please`} />) /* prettier-ignore */
+        }
+      </Scroll>
     </ScrollControls>
   );
 }
