5b31e816e0b003361d0a98e085732cb4dd6a8194
Yasin Khan <yasinkhan@Yasins-Mac-mini.local>
2024-12-27 07:58:37 +0600

    final product

---
+++ src/App.jsx
@@ -0,0 +1,149 @@
+import * as THREE from "three";
+import { useEffect, useRef, useState } from "react";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import { Image, ScrollControls, Scroll, useScroll } from "@react-three/drei";
+import { proxy, useSnapshot } from "valtio";
+import { easing } from "maath";
+
+const state = proxy({
+  clicked: null,
+});
+
+const material = new THREE.LineBasicMaterial({ color: "white" });
+const geometry = new THREE.BufferGeometry().setFromPoints([
+  new THREE.Vector3(0, -0.5, 0),
+  new THREE.Vector3(0, 0.5, 0),
+]);
+
+const urls = [
+  "https://images.unsplash.com/photo-1519710164239-da123dc03ef4?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1524758631624-e2822e304c36?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1487015307662-6ce6210680f1?q=80&w=1285&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://plus.unsplash.com/premium_photo-1688125414593-391cf90f3103?q=80&w=1064&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1486946255434-2466348c2166?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://plus.unsplash.com/premium_photo-1671269942393-ab3372a09ce9?q=80&w=1209&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1491926626787-62db157af940?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1611269154421-4e27233ac5c7?q=80&w=1065&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1537212013010-de9c76f8f0ae?q=80&w=1035&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1511401139252-f158d3209c17?q=80&w=1035&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://plus.unsplash.com/premium_photo-1681980020984-2700a72e56c4?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1445510861639-5651173bc5d5?q=80&w=1059&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://plus.unsplash.com/premium_photo-1673548917423-073963e7afc9?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://plus.unsplash.com/premium_photo-1683140589868-4f9b38110050?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://plus.unsplash.com/premium_photo-1682410455752-1007e8cea553?q=80&w=1026&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1612372606404-0ab33e7187ee?q=80&w=1278&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1679558916026-c5a87d6cd490?q=80&w=1227&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://plus.unsplash.com/premium_photo-1670076735810-26d92491df5c?q=80&w=1116&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1507904953637-96429a46671a?q=80&w=1064&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1551298698-66b830a4f11c?q=80&w=1015&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+];
+
+function Minimap() {
+  const ref = useRef();
+  const scroll = useScroll();
+  const { height } = useThree((state) => state.viewport);
+  useFrame((state, delta) => {
+    ref.current.children.forEach((child, index) => {
+      const y = scroll.curve(
+        index / urls.length - 1.5 / urls.length,
+        4 / urls.length
+      );
+      easing.damp(child.scale, "y", 0.15 + y / 6, 0.15, delta);
+    });
+  });
+  return (
+    <group ref={ref}>
+      {urls.map((_, i) => (
+        <line
+          key={i}
+          geometry={geometry}
+          material={material}
+          position={[i * 0.06 - urls.length * 0.03, -height / 2 + 0.6, 0]}
+        />
+      ))}
+    </group>
+  );
+}
+
+function Item({ index, position, scale, c = new THREE.Color(), ...props }) {
+  const ref = useRef();
+  const scroll = useScroll();
+  const { clicked } = useSnapshot(state);
+  const [hovered, hover] = useState(false);
+  const click = () => (state.clicked = index === clicked ? null : index);
+  const over = () => hover(true);
+  const out = () => hover(false);
+
+  useFrame((state, delta) => {
+    const y = scroll.curve(
+      index / urls.length - 1.5 / urls.length,
+      4 / urls.length
+    );
+    easing.damp3(
+      ref.current.scale,
+      [clicked === index ? 4.7 : scale[0], clicked === index ? 5 : 4 + y, 1],
+      0.15,
+      delta
+    );
+    ref.current.material.scale = [ref.current.scale.x, ref.current.scale.y];
+    if (clicked !== null && index < clicked)
+      easing.damp(ref.current.position, "x", position[0] - 2, 0.15, delta);
+    if (clicked !== null && index > clicked)
+      easing.damp(ref.current.position, "x", position[0] + 2, 0.15, delta);
+    if (clicked === null || clicked === index)
+      easing.damp(ref.current.position, "x", position[0], 0.15, delta);
+    easing.damp(
+      ref.current.material,
+      "grayscale",
+      hovered || clicked === index ? 0 : Math.max(0, 1 - y),
+      0.15,
+      delta
+    );
+    easing.dampC(
+      ref.current.material.color,
+      hovered || clicked === index ? "white" : "#aaa",
+      hovered ? 0.3 : 0.15,
+      delta
+    );
+  });
+
+  return (
+    <Image
+      ref={ref}
+      {...props}
+      position={position}
+      scale={scale}
+      onClick={click}
+      onPointerOver={over}
+      onPointerOut={out}
+    />
+  );
+}
+
+function Items({ w = 0.7, gap = 0.15 }) {
+  const { width } = useThree((state) => state.viewport);
+  const xW = w + gap;
+  const pages = (width - xW + urls.length * xW) / width;
+
+  return (
+    <ScrollControls horizontal damping={0.1} pages={pages}>
+      <Minimap />
+      <Scroll>
+        {
+          urls.map((url, i) => <Item key={i} index={i} position={[i * xW, 0, 0]} scale={[w, 4, 1]} url={`${url}?not-from-cache-please`} />) /* prettier-ignore */
+        }
+      </Scroll>
+    </ScrollControls>
+  );
+}
+
+export const App = () => (
+  <Canvas
+    gl={{ antialias: false }} // at last
+    onPointerMissed={() => (state.clicked = null)} //at last
+  >
+    <Items />
+  </Canvas>
+);
+
+export default App;
